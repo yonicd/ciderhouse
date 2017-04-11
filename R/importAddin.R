87a6253e5e1a4880b955f4c1d08be289cf50b02b
jonathans <yonicd@gmail.com>
2017-04-11 15:49:06 -0400

    add addin for makeImport

---
+++ R/importAddin.R
@@ -0,0 +1,15 @@
+#' @importFrom rstudioapi getActiveDocumentContext
+importAddin <- function() {
+  require(YSmisc)
+  # Get the document context.
+  context <- rstudioapi::getActiveDocumentContext()
+  
+  if(is.null(context$path)){
+    f<-tempfile()
+    cat(context$contents,file=f)
+    context$path=f
+  }
+  
+  makeImport(context$path,print = TRUE,format = 'oxygen')
+  if(exists('f')) unlink(f)
+}

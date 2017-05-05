989c1b56a579d910d69b4e354ff6fde26ff52d67
jonathans <yonicd@gmail.com>
2017-05-04 20:16:11 -0400

    fix reshape_description

--- gitLogs/processJSON.R
+++ gitLogs/processJSON.R
@@ -40,24 +40,34 @@ getPackagesWithTitle <- function() {
   db[, c("Package", "Title")]
 }
 
-reshape_description=function(parse_descriptions){
-  a=plyr::mdply(parse_descriptions[!is.na(parse_descriptions)],.fun = function(x) {
+reshape_description=function(jsons,jsons.names){
+  
+  a=plyr::mdply(jsons,.fun = function(x) {
     data.frame(fromJSON(x)[[1]],stringsAsFactors = FALSE)},.progress = 'text')
+  
   a$ON_CRAN=ifelse(a$Package%in%cran_current[,1],'CRAN_GITHUB','ONLY_GITHUB')
-  a$repo=names(jsons)[!is.na(parse_descriptions)][as.numeric(a$X1)]
+  
+  a$repo=jsons.names[as.numeric(a$X1)]
+  
   a1=a%>%dplyr::select(X1,ON_CRAN,repo,Package,Title,Author,Description,Depends,Imports,Suggests,LinkingTo)%>%
     reshape2::melt(.,id= head(names(.),-4))%>%dplyr::filter(!is.na(value))
   
   # clean a bit more....
   a2=a1%>%plyr::ddply(head(names(a1),-1),.fun=function(x){
-    data.frame(value=gsub('^\\s+|\\s+$|\\s+\\((.*?)\\)|\\((.*?)\\)|\\b.1\\b|^s: ','',strsplit(x$value,',')[[1]]),stringsAsFactors = FALSE)
+    data.frame(value=gsub(pattern = '^\\s+|\\s+$|\\s+\\((.*?)\\)|\\((.*?)\\)|\\b.1\\b|^s: ',
+                          replacement = '',
+                          x = strsplit(x$value,',')[[1]]
+                          ),
+               stringsAsFactors = FALSE)
   },.progress = 'text')%>%dplyr::filter(!grepl(':|NULL',value))
   
   # reshape for rankings
-  a3<-a2%>%plyr::dlply(.variables = c('ON_CRAN'),.fun=function(df){ df%>%dplyr::count(variable,value)%>%dplyr::arrange(variable,desc(n))%>%
+  a3<-a2%>%plyr::dlply(.variables = c('ON_CRAN'),.fun=function(df){ 
+      df%>%dplyr::count(variable,value)%>%dplyr::arrange(variable,desc(n))%>%
       dplyr::group_by(variable)%>%dplyr::do(.,cbind(rank=1:nrow(.),.))%>%
       dplyr::mutate(value=sprintf('%s (%s)',value,n))%>%
-      reshape2::dcast(rank~variable,value.var='value')})
+      reshape2::dcast(rank~variable,value.var='value')
+    })
   l=list(raw=a,clean=a2,ranking=a3)
   
   return(l)
@@ -90,12 +100,14 @@ cran_current=getPackagesWithTitle()
 
 JSONS=c(JSONS,parse_descriptions[!is.na(parse_descriptions)])
 
-out=reshape_description(JSONS)
+JSONS.names=sapply(JSONS,function(x) names(fromJSON(x)))
+names(JSONS.names)<-NULL
+
+out=reshape_description(JSONS,JSONS.names)
 
 #print top 10
 lapply(out$ranking,function(x) head(x,10))
 
-JSONS=c(JSONS,parse_descriptions[!is.na(parse_descriptions)])
 save(JSONS,file='gitLogs/gitLogs.rdata')
-JSON.names=sapply(JSONS,function(x) names(fromJSON(x)))
+
 save(JSONS.names,file='gitLogs/current_repo_list.Rdata')

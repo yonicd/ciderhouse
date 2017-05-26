7ea84df5e38901f5df46fadcd0684e12fccb9ea6
jonathans <yonicd@gmail.com>
2017-05-26 15:54:39 -0400

    replace svn with gh api

--- R/show_repo.R
+++ R/show_repo.R
@@ -1,6 +1,7 @@
 #' @title Visually inspect structure of git repository before initial fetch
 #' @description Visually inpsect the directory structure of it before cloning/fetching/pulling
 #' @param path character, Path to root directory of git repository or a name of a github repository, Default: setwd()
+#' @param gh_branch character, alias of the github branch to retrieve directory structure, Default: 'master'
 #' @param layout character, Layout of d3Tree output collapse, cartesian, radial Default: 'collapse'
 #' @details 
 #' By default path assumes a local address, if path is a valid repository name eg 'tidyverse/glue' 
@@ -14,15 +15,21 @@
 #' @importFrom httr http_error
 #' @examples 
 #' show_repo('tidyverse/glue')
+#' show_repo('tidyverse/glue',gh_branch='named_args')
 #' @seealso
 #'  \code{\link[d3Tree]{d3tree}}
-show_repo <- function(path=getwd(),layout='collapse'){
+show_repo <- function(path=getwd(),gh_branch='master',layout='collapse'){
   this_wd <- getwd()
   if(!dir.exists(path)){
     uri <- sprintf('https://github.com/%s.git',path)
     if (httr::http_error(uri))
       stop(sprintf("github repo: %s not found", uri))
-    pathout <- system(sprintf('svn ls %s/branches/master -R',uri),intern=TRUE)[-1]
+    #pathout <- system(sprintf('svn ls %s/branches/master -R',uri),intern=TRUE)[-1]
+    
+    tr<-httr::content(httr::GET(sprintf('https://api.github.com/repos/%s/git/trees/%s%s',path,gh_branch,'?recursive=1')))$tree
+    s=sapply(tr,function(x) if(x$mode!='040000') x$path)
+    pathout<-unlist(s)
+    
   }else{
     setwd(tools::file_path_as_absolute(path))
     pathout <- system('git ls-tree -r HEAD --name-only',intern=TRUE)    
@@ -31,9 +38,6 @@ show_repo <- function(path=getwd(),layout='collapse'){
   x <- lapply(strsplit(pathout, "/"), function(z) as.data.frame(t(z)))
   x <- plyr::rbind.fill(x)
   x$depth <- apply(x,1,function(y) sum(!is.na(y)))
-  if(!dir.exists(path)){
-    x<-x[(x$depth-c(tail(x$depth,-1),0))!=-1,]
-  } 
   setwd(this_wd)
   htmltools::html_print(d3Tree::d3tree(list(root = d3Tree::df2tree(rootname='repo',struct=x),layout = layout)))
   invisible(x)

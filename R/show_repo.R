957c5c9eeb1abb5a9ed1d5c777a039f97f23000c
jonathans <yonicd@gmail.com>
2017-05-26 11:36:11 -0400

    keep archive.txt in folder and add param to update it

--- R/show_repo.R
+++ R/show_repo.R
@@ -1,6 +1,7 @@
 #' @title Visually inspect structure of git repository before initial fetch
 #' @description After adding a remote visually inpsect the directory structure of it before fetching/pulling
 #' @param layout charaacter, Layout of d3Tree output collapse, cartesian, radial Default: 'collapse'
+#' @param update_archive boolean, update the archive file, Default: FALSE
 #' @return nothing
 #' @export
 #' @import dplyr
@@ -8,14 +9,20 @@
 #' @importFrom d3Tree d3tree df2tree
 #' @seealso
 #'  \code{\link[d3Tree]{d3tree}}
-show_repo <- function(layout='collapse'){
-  if(.Platform$OS.type=='windows'){
-    shell('git archive --format=tar HEAD | tar t > _archive_.txt')
-  }else{
-    system('git archive --format=tar HEAD | tar t > _archive_.txt') 
+show_repo <- function(layout='collapse',update_archive=FALSE){
+  if(!file.exists('_archive_.txt')|update_archive){
+    if(.Platform$OS.type=='windows'){
+      shell('git archive --format=tar HEAD | tar t > _archive_.txt')
+    }else{
+      system('git archive --format=tar HEAD | tar t > _archive_.txt') 
+    }
+    
+    if(!any(grepl('_archive_.txt',readLines('.gitignore')))) cat('_archive_.txt',file='.gitignore',sep='\n',append = TRUE)
+    
+    if(file.exists('.Rbuildignore')&!any(grepl('_archive_.txt',readLines('.Rbuildignore')))) cat('_archive_.txt',file='.Rbuildignore',sep='\n',append = TRUE)
+    
   }
   path<-readLines('_archive_.txt')
-  system('rm _archive_.txt')
   x <- lapply(strsplit(path, "/"), function(z) as.data.frame(t(z)))
   x <- plyr::rbind.fill(x)
   x$depth <- apply(x,1,function(y) sum(!is.na(y)))
